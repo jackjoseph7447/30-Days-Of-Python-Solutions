d96b2a939106418c2b91b6ed453b894527ed199a
John Joseph II <jackjoseph7447@gmail.com>
2024-07-10 16:17:21 -0400

    created folder and variables code

---
+++ 02_Day_Variables_builtin_functions/day_2/variables.py
@@ -0,0 +1,18 @@
+# Day 2: 30 Days of python programming
+
+first_name = 'John'
+last_name = 'Joseph'
+full_name = 'John Joseph'
+country = 'United States'
+city = 'Philadelphia'
+age = 22
+year = 2024
+is_married = False
+is_true = True
+is_light_on = True
+my_info = {
+    full_name,
+    country,
+    city,
+    age
+}

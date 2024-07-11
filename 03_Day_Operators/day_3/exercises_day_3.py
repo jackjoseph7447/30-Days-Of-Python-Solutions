8af80eff1dfdd8d7cd671d3774037dc84b7f4e2b
John Joseph II <jackjoseph7447@gmail.com>
2024-07-10 23:10:14 -0400

    One more question

--- 03_Day_Operators/day_3/exercises_day_3.py
+++ 03_Day_Operators/day_3/exercises_day_3.py
@@ -1,22 +1,67 @@
 # Day 3 Exercises
 
-age = 22
-height = 6.25
-complex = 1j + 2
+# age = 22
+# height = 6.25
+# complex = 1j + 2
 
-user_tri_base = input('Enter the triangle base ')
-user_tri_height = input('Enter the triangle height ')
-user_tri_area = 0.5 * int(user_tri_base) * int(user_tri_height)
-print('The area of the triangle is', user_tri_area)
+# user_tri_base = input('Enter the triangle base ')
+# user_tri_height = input('Enter the triangle height ')
+# user_tri_area = 0.5 * int(user_tri_base) * int(user_tri_height)
+# print('The area of the triangle is', user_tri_area)
 
-user_side_a = input('Side A: ')
-user_side_b = input('Side B: ')
-user_side_c = input('Side C: ')
-user_tri_sides_prim = int(user_side_a) + int(user_side_b) + int(user_side_c)
-print('The perimeter of the triangle is', user_tri_sides_prim)
+# user_side_a = input('Side A: ')
+# user_side_b = input('Side B: ')
+# user_side_c = input('Side C: ')
+# user_tri_sides_prim = int(user_side_a) + int(user_side_b) + int(user_side_c)
+# print('The perimeter of the triangle is', user_tri_sides_prim)
 
-user_rec_width = int(input('Rectangle Width: '))
-user_rec_height = int(input('Rectangle Height: ')) #Can make int when in taking values
-user_rec_area = user_rec_height * user_rec_width
-user_rec_prim = (user_rec_height * 2) + (user_rec_width * 2) #Rework
-print('The area of the rectangle is', user_rec_area)
+# user_rec_width = int(input('Rectangle Width: '))
+# user_rec_height = int(input('Rectangle Height: ')) #Can make int when in taking values
+# user_rec_area = user_rec_height * user_rec_width
+# user_rec_prim = 2 * (user_rec_height + user_rec_width)
+# print('The area of the rectangle is', user_rec_area)
+# print('The perimeter of the rectangle is', user_rec_prim)
+
+# pi = 3.14
+# user_radius = int(input('Enter a radius: '))
+# circle_area = pi * user_radius ** 2
+# circle_circum = 2 * pi * user_radius
+# print('The circle area is', circle_area)
+# print('The circle circumference is', circle_circum)
+
+#Skipping 8 - 11
+
+# print(len('python') != len('dragon'))
+
+# print('on' in 'python' and 'on' in 'dragon')
+
+# print('jargon' in 'I hope this course is not full of jargon')
+
+# print('on' not in 'python' and 'on' not in 'dragon')
+
+# length = len('python')
+# print(len('python'))
+# print(float(length))
+# print(str(length))
+
+# print('=|')
+
+# print(7 // 3 == int(2.7))
+
+# print('10' == 10)
+
+# #print(int('9.8') == 10) Error
+
+# user_hours = int(input('How many hours? '))
+# user_rate = int(input('How much do you make an hour? '))
+# weekly_rate = user_hours * user_rate
+# print('Your weekly income is', weekly_rate)
+
+# years = int(input('How many years have you lived? '))
+# seconds = years * 365 * 24 * 60 * 60 #1 year x 1 day x 1 hour x 1 second
+# print('you lived', seconds, 'seconds')
+
+table = {1, 2, 3, 4, 5}
+print(table)
+
+# n 1 n n^2 n^3
